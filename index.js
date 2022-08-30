4f62ce206524bb233815314c519df6b9dbf4a9bf
Jochen Kalmbach <35481830+JochenKalmbachHomag@users.noreply.github.com>
2022-08-30 17:23:00 +0200

    Adjusted to latest change in "BufferGeometry" (addAttribute is now deprecated)

--- index.js
+++ index.js
@@ -1144,7 +1144,7 @@ var _GLTFExporter = (function(){
 
 						console.warn( 'THREE.GLTFExporter: Creating normalized normal attribute from the non-normalized one.' );
 
-						geometry.addAttribute( 'normal', createNormalizedNormalAttribute( originalNormal ) );
+						geometry.setAttribute( 'normal', createNormalizedNormalAttribute( originalNormal ) );
 
 					}
 
@@ -1179,7 +1179,7 @@ var _GLTFExporter = (function(){
 
 					}
 
-					if ( originalNormal !== undefined ) geometry.addAttribute( 'normal', originalNormal );
+					if ( originalNormal !== undefined ) geometry.setAttribute( 'normal', originalNormal );
 
 					// Skip if no exportable attributes found
 					if ( Object.keys( attributes ).length === 0 ) {
